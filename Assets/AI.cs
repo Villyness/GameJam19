efa0e0d8cd6a3a131bb9778899fa800f942383aa
Venus <inkyinked@gmail.com>
2019-09-09 17:53:38 +1000

    Base AI movement completed.

---
+++ Assets/AI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    // This is the base script for the AI ships used by the player
+    // Setting up...
+    public string name;
+    public Transform[] path;
+    public int currentPoint = 0;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != path[currentPoint].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, path[currentPoint].transform.position,
+                speed * Time.deltaTime);
+        }
+
+        if (transform.position == path[currentPoint].position)
+        {
+            currentPoint++;
+        }
+
+        if (currentPoint >= path.Length)
+        {
+            currentPoint = 0;
+        }
+    }
+}
